84c08ee30de4adbf6b796df843a2481476e4fc39
kartik30R <kartik30rehan@gmail.com>
2024-09-30 13:47:32 +0530

    .

--- lib/view/screens/app/explore_screen.dart
+++ lib/view/screens/app/explore_screen.dart
@@ -2,7 +2,6 @@ import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:newzent/model/news/news_model.dart';
-import 'package:newzent/resources/constants/dimension/app_dimension.dart';
 import 'package:newzent/resources/constants/string/app_string.dart';
 import 'package:newzent/view/widgets/news_card.dart';
 import 'package:newzent/view/widgets/news_tile.dart';
@@ -38,71 +37,68 @@ class ExploreScreen extends StatelessWidget {
           title: SizedBox(height: 20,child: Image.asset('assets/logo.png',)),
         ),
             body: SingleChildScrollView(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  const SizedBox(height: 21,),
-                   Text(
-              "Explore",
-              style: Theme.of(context).textTheme.titleLarge,
-                            ),
-                            const SizedBox(height: 16),
-                            Padding(
-                              padding:  EdgeInsets.symmetric(horizontal:AppDimension().defaultMargin),
-                              child: const TextField(decoration: InputDecoration(
-                                            hintText: 
-                               'search',
-                               prefixIcon: Icon(Icons.search)
-                              ),),
-                            ),
-                            const SizedBox(height: 21,),
-                             Padding(
-                              padding:  EdgeInsets.symmetric(horizontal:AppDimension().defaultMargin),
-                               child: Text("Editor's Choice",style: Theme.of(context).textTheme.bodyMedium,),
-                             ),
-                            const SizedBox(height: 8,),
-                  CarouselSlider.builder(
-                    itemCount: 5,
-                    itemBuilder: (context, index, realIndex) {
-                      return NewsCard(news: newsController.topNews[index]);
-                    },
-                    options: CarouselOptions(
-                      height: 235,
-                      viewportFraction: .8,
-                      enlargeStrategy: CenterPageEnlargeStrategy.scale,
-                      enableInfiniteScroll: false,
-                      initialPage: 2,
-                      autoPlay: true,
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const SizedBox(height: 21,),
+                     Text(
+                "Explore",
+                style: Theme.of(context).textTheme.titleLarge,
+              ),
+              const SizedBox(height: 16),
+              const TextField(decoration: InputDecoration(
+                hintText: 
+               'search',
+               prefixIcon: Icon(Icons.search)
+              ),),
+              const SizedBox(height: 21,),
+               Text("Editor's Choice",style: Theme.of(context).textTheme.bodyMedium,),
+              const SizedBox(height: 8,),
+                    CarouselSlider.builder(
+                      itemCount: 5,
+                      itemBuilder: (context, index, realIndex) {
+                        return NewsCard(news: newsController.topNews[index]);
+                      },
+                      options: CarouselOptions(
+                        height: 235,
+                        viewportFraction: .8,
+                        enlargeStrategy: CenterPageEnlargeStrategy.scale,
+                        enableInfiniteScroll: false,
+                        initialPage: 2,
+                        autoPlay: true,
+                      ),
                     ),
-                  ),
-                  TabBar(
-                    isScrollable: true,
-                    tabs: [
-                      const Tab(text: 'Top'),
-                      ...AppString.categories.map((category) {
-                        return Tab(text: category.capitalizeFirst);
-                      }).toList(),
-                    ],
-                  ),
-                  Container(
-                    height: MediaQuery.of(context).size.height -
-                        kToolbarHeight +
-                        700, 
-                    child: TabBarView(
-                      controller: tabController,
-                      children: [
-                        _buildNewsList(newsController.topNews, null, newsController.isLoadingTopNews),
+                    TabBar(
+                      isScrollable: true,
+                      tabs: [
+                        const Tab(text: 'Top'),
                         ...AppString.categories.map((category) {
-                          return _buildNewsList(
-                            newsController.getNewsListByCategory(category),
-                            category,
-                            newsController.getLoadingStateByCategory(category),
-                          );
+                          return Tab(text: category.capitalizeFirst);
                         }).toList(),
                       ],
                     ),
-                  ),
-                ],
+                    Container(
+                      height: MediaQuery.of(context).size.height -
+                          kToolbarHeight +
+                          700, 
+                      child: TabBarView(
+                        controller: tabController,
+                        children: [
+                          _buildNewsList(newsController.topNews, null, newsController.isLoadingTopNews),
+                          ...AppString.categories.map((category) {
+                            return _buildNewsList(
+                              newsController.getNewsListByCategory(category),
+                              category,
+                              newsController.getLoadingStateByCategory(category),
+                            );
+                          }).toList(),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
               ),
             ),
           );
